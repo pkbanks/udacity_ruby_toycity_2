18efeede166d824a51f5ef4e6c0ff57be7fb34a2
PK Banks <pkbanks@gmail.com>
2016-04-04 21:55:47 -0400

    finished a few basic methods that don't handle any logic, mostly print headers.  now about to start the logic portion of the process

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,13 @@
 require 'json'
+require 'date'
+
+# things to keep in mind:
+# • Keep your methods short.
+# • include:
+# 		- a method that has default values for arguments.
+#			- a method that has an options hash.
+# • use return values appropriately
+# • incorporate methods that make semantic sense
 
 def setup_files
 	# taken from udacity instructions
@@ -7,17 +16,53 @@ def setup_files
 	file = File.read(path)
 	$products_hash = JSON.parse(file)							# $ denotes a global variable so that it can be accessed outside of the method
 	$report_file = File.new("report.txt", "w+")  	# $ denotes a global variable so that it can be accessed outside of the method
+end
 
 def create_report
 	# will be called by the start method
-	puts 'hoho'
+	
+	# Print "Sales Report" in ascii art
+	print_sales_report_header
+
+	# Print today's date
+	puts date_today
+
+	# Print "Products" in ascii art
+	prints_products_report_header
+
+end
 
 
-# Print "Sales Report" in ascii art
+def print_sales_report_header
+	# prints a sales report header in ascii art
+	puts "  ____        _             ____                       _   "
+	puts " / ___|  __ _| | ___  ___  |  _ \\ ___ _ __   ___  _ __| |_ "
+	puts " \\___ \\ / _` | |/ _ \\/ __| | |_) / _ \\ '_ \\ / _ \\| '__| __|"
+	puts "  ___) | (_| | |  __/\\__ \\ |  _ <  __/ |_) | (_) | |  | |_ "
+	puts " |____/ \\__,_|_|\\___||___/ |_| \\_\\___| .__/ \\___/|_|   \\__|"
+	puts "                                     |_|                   "
+end
 
-# Print today's date
+def date_today()
+	# returns today's date
+	# to print, simply call puts to this method
+	t = Date.today
+	return t
+end
 
-# Print "Products" in ascii art
+def prints_products_report_header
+	# prints a products report header in ascii art
+	puts "                     _            _       "
+	puts "                    | |          | |      "
+	puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
+	puts "| '_ \\| '__/ _ \\ / _` | | | |/ __| __/ __|"
+	puts "| |_) | | | (_) | (_| | |_| | (__| |_\\__ \\"
+	puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
+	puts "| |                                       "
+	puts "|_|                                       "
+
+end
+
 
 # For each product in the data set:
 	# Print the name of the toy
@@ -41,3 +86,4 @@ def start
 end
 
 start # call start method to trigger report generation
+
